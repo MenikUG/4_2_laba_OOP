367cf412665f6b0219513c1a7681fba7b0889e2e
MenikUG <ivan2014159@gmail.com>
2020-11-27 17:12:04 +0500

    Initial commit

---
+++ 4_2_laba_OOP/4.2 Laba OOP.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace _4_2_laba_OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1 = new System.Windows.Forms.NumericUpDown();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.num2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.num1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.num1.Location = new System.Drawing.Point(8, 12);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(244, 34);
+            this.num1.TabIndex = 1;
+            this.num1.ValueChanged += new System.EventHandler(this.num1_ValueChanged);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(9, 49);
+            this.hScrollBar1.Maximum = 109;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(243, 35);
+            this.hScrollBar1.TabIndex = 3;
+            this.hScrollBar1.ValueChanged += new System.EventHandler(this.hScrollBar1_ValueChanged);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(9, 98);
+            this.maskedTextBox1.Mask = "000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(42, 34);
+            this.maskedTextBox1.TabIndex = 4;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            this.maskedTextBox1.Click += new System.EventHandler(this.maskedTextBox1_Click);
+            this.maskedTextBox1.TextChanged += new System.EventHandler(this.maskedTextBox1_TextChanged);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maskedTextBox2.Location = new System.Drawing.Point(326, 98);
+            this.maskedTextBox2.Mask = "000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(42, 34);
+            this.maskedTextBox2.TabIndex = 7;
+            this.maskedTextBox2.Text = "1";
+            this.maskedTextBox2.ValidatingType = typeof(int);
+            this.maskedTextBox2.Click += new System.EventHandler(this.maskedTextBox2_Click);
+            this.maskedTextBox2.TextChanged += new System.EventHandler(this.maskedTextBox2_TextChanged);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(326, 49);
+            this.hScrollBar2.Maximum = 110;
+            this.hScrollBar2.Minimum = 1;
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(243, 35);
+            this.hScrollBar2.TabIndex = 6;
+            this.hScrollBar2.Value = 1;
+            this.hScrollBar2.ValueChanged += new System.EventHandler(this.hScrollBar2_ValueChanged);
+            // 
+            // num2
+            // 
+            this.num2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.num2.Location = new System.Drawing.Point(325, 12);
+            this.num2.Maximum = new decimal(new int[] {
+            101,
+            0,
+            0,
+            0});
+            this.num2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(244, 34);
+            this.num2.TabIndex = 5;
+            this.num2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num2.ValueChanged += new System.EventHandler(this.num2_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 533);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.num1);
+            this.Name = "Form1";
+            this.Text = "4.2 Laba OOP";
+            ((System.ComponentModel.ISupportInitialize)(this.num1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown num1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.NumericUpDown num2;
+    }
+}
+
---
+++ 4_2_laba_OOP/4.2 Laba OOP.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_2_laba_OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            int num = hScrollBar1.Value;
+            num1.Value = num;
+            maskedTextBox1.Text = num.ToString();
+            ++num;
+            num2.Value = num;
+            maskedTextBox2.Text = num.ToString();
+        }
+
+        private void num1_ValueChanged(object sender, EventArgs e)
+        {
+            int num = (int)num1.Value;
+            hScrollBar1.Value = num;
+            maskedTextBox1.Text = num.ToString();
+            ++num;
+            hScrollBar2.Value = num;
+            maskedTextBox2.Text = num.ToString();
+        }
+
+        private void maskedTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text != "")
+            {
+                int num = Convert.ToInt32(maskedTextBox1.Text);
+                if (num <= 100)
+                {
+                    num1.Value = num;
+                    hScrollBar1.Value = num;
+                    ++num;
+                    num2.Value = num;
+                    hScrollBar2.Value = num;
+                }
+            }
+        }
+
+        private void maskedTextBox1_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "";
+        }
+
+        private void num2_ValueChanged(object sender, EventArgs e)
+        {
+            int num = (int)num2.Value;
+            hScrollBar2.Value = num;
+            maskedTextBox2.Text = num.ToString();
+            --num;
+            hScrollBar1.Value = num;
+            maskedTextBox1.Text = num.ToString();
+        }
+
+        private void hScrollBar2_ValueChanged(object sender, EventArgs e)
+        {
+            int num = hScrollBar2.Value;
+            num2.Value = num;
+            maskedTextBox2.Text = num.ToString();
+            --num;
+            num1.Value = num;
+            maskedTextBox1.Text = num.ToString();
+        }
+
+        private void maskedTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text != "")
+            {
+                int num = Convert.ToInt32(maskedTextBox2.Text);
+                if (num <= 101)
+                {
+                    num2.Value = num;
+                    hScrollBar2.Value = num;
+                    --num;
+                    num1.Value = num;
+                    hScrollBar1.Value = num;
+                }
+            }
+        }
+
+        private void maskedTextBox2_Click(object sender, EventArgs e)
+        {
+            maskedTextBox2.Text = "";
+        }
+    }
+}
